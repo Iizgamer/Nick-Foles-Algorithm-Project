51d28bb71250d25ad6637e6787309ae847e60252
Nick <nickfoles09@icloud.com>
2026-04-21 14:11:54 -0400

    i hate indy car

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const startBtn = document.getElementById('startBtn');
+let xInput = document.getElementById('xInput');
+let yInput = document.getElementById('yInput');
 
 startBtn.addEventListener('mouseenter', (e) => {
-    startBtn.style.background = 'white'   
+    startBtn.style.background = '#afafaf';  
 });
+
+startBtn.addEventListener('mouseleave', (e) => {
+    startBtn.style.background = '#838383'
+});
+
+class Cell {
+    constructor() {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+startBtn.addEventListener('click', function() {
+    console.log('yInput.value')
+})
